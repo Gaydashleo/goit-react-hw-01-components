eb4b0bbfd12d4154df53eca86ab23dc865c11e92
Leonid Haidash <Gaydashleo@gmail.com>
2022-09-05 18:11:55 +0300

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,12 @@
 export default function Profile({ username, tag, location, avatar, stats }) {
   return (
-    <div class="profile">
+<div class="profile">
   <div class="description">
-    <img src = {https://cdn-icons-png.flaticon.com/512/1077/1077012.png}/>
-      
+    <img
+      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
       alt="User avatar"
       class="avatar"
-    
+    />
     <p class="name">Petra Marica</p>
     <p class="tag">@pmarica</p>
     <p class="location">Salvador, Brasil</p>
