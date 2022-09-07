4fbde9b0ce6a640e14edcb0d141453107adb3717
Leonid Haidash <Gaydashleo@gmail.com>
2022-09-07 11:38:15 +0300

    usercardadd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { UserCard } from 'components/UserCard/UserCard';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <UserCard/>
+      {/* React homework template */}
     </div>
   );
 };
---
+++ src/components/UserCard/UserCard.jsx
@@ -0,0 +1,5 @@
+import css from './UserCard.module.css';
+
+export const UserCard = () => {
+  return <div className={css.UserCard}> User Card</div>;
+};
