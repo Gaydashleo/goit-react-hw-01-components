0a003f8d04b3a64d7959ac1d03cdcaa2b4318941
Leonid Haidash <Gaydashleo@gmail.com>
2022-09-09 11:18:49 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,23 +6,18 @@ export default function Statistics({title, stats }) {
     <section className={css.statistics}>
       <h2 className={css.title}>{title}</h2>
 
-      <ul className={css.statList }>
-        <li className="item">
-          <span className="label">.docx</span>
-          <span className="percentage">4%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp3</span>
-          <span class="percentage">14%</span>
-        </li>
-        <li class="item">
-          <span class="label">.pdf</span>
-          <span class="percentage">41%</span>
-        </li>
-        <li class="item">
-          <span class="label">.mp4</span>
-          <span class="percentage">12%</span>
-        </li>
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={css.item}
+              key={id}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
