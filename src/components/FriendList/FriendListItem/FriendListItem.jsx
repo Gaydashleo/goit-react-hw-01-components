4f7f7cd926b3428c4eb0aa5fa47039f62cc9e64f
Leonid Haidash <Gaydashleo@gmail.com>
2022-09-10 19:23:45 +0300

    friendlistend

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -4,7 +4,7 @@ import css from "./FriendListItem.module.css";
 function FriendListItem({ isOnline, avatar, name }) {
   return (
     <li className={css.item}>
-      <span className={isOnline? css.status__true: css.status__false}></span>
+      <span className={isOnline? css.statusOnline: css.statusOfline}></span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
